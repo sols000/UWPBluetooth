019c01df40f0232ea8d0c8e9dc717dc1622c6e22
zhaobaojun <baojun@nolorvr.com>
2019-11-19 20:42:16 +0800

    xiugai

--- UWPBluetooth/MainPage.xaml.cs
+++ UWPBluetooth/MainPage.xaml.cs
@@ -34,9 +34,9 @@ namespace UWPBluetooth
             Debug.WriteLine("主窗口开始加载");
 
             AdapterStatus Astatus = CrossBleAdapter.Current.Status;
-            CrossBleAdapter.Current.WhenStatusChanged().Subscribe(status => 
+            CrossBleAdapter.Current.WhenStatusChanged().Subscribe(status =>
             {
-                Debug.WriteLine("状态更新："+ status.ToString());
+                Debug.WriteLine("状态更新：" + status.ToString());
             });
             Debug.WriteLine("主窗口加载完成");
         }
@@ -49,22 +49,64 @@ namespace UWPBluetooth
             {
                 // do something with it
                 // the scanresult contains the device, RSSI, and advertisement packet
-                Debug.WriteLine("扫描到设备：" + scanResult.Device.Uuid);
+                Debug.WriteLine("扫描到设备：" + scanResult.Device.Uuid + "扫描到设备：" + scanResult.Device.Name);
             });
             //scanner.Dispose(); // to stop scanning
         }
-
+        IDevice currdev = null;
         private void OnScanClicked2(object sender, RoutedEventArgs e)
         {
-            Debug.WriteLine("定向扫描");
+
             CrossBleAdapter.Current.Scan(new ScanConfig
             {
                 //ScanType = BleScanType.Background,
                 ServiceUuids = { new Guid("48EB9001-F352-5FA0-9B06-8FCAA22602CF") }
             }).Subscribe(scanResult =>
             {
-                Debug.WriteLine("扫描到设备：" + scanResult.Device.Name);
+
+                this.Invoke(() =>
+                {
+                    TXT.Text = "扫描到设备：" + scanResult.Device.Name;
+                });
+
+                currdev = scanResult.Device;
+
             });
+
+        }
+        public async void Invoke(Action action, Windows.UI.Core.CoreDispatcherPriority Priority = Windows.UI.Core.CoreDispatcherPriority.Normal)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Priority, () => { action(); });
+
+        }
+       
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //currdev.BeginReliableWriteTransaction().Write()
+            IObservable<IGattCharacteristic> array = currdev.GetCharacteristicsForService(new Guid("48EB9002-F352-5FA0-9B06-8FCAA22602CF"));
+            // TXT.Text = array.ToString();
+
+            //  currdev.WhenAnyCharacteristicDiscovered().Subscribe(characteristic =>
+            //{
+            //      // read, write, or subscribe to notifications here
+            //      var result = characteristic.Read(); // use result.Data to see response
+            //                                          //await characteristic.Write(bytes);
+
+            //      characteristic.EnableNotifications();
+            //    characteristic.WhenNotificationReceived().Subscribe(result1 =>
+            //    {
+            //          //result.Data to get at response
+            //          TXT.Text = result1.Characteristic.Value.ToString();
+            //    });
+            //});
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            currdev.Connect();
+           
         }
     }
 }
